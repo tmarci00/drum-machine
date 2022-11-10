f1944aa8ad2234c90df2a73b0b15d4dd508c5d9b
tmarci00 <tothmarcell7@gmail.com>
2022-11-10 15:47:16 +0100

    all tests passing, volume not functional

--- src/App.js
+++ src/App.js
@@ -1,23 +1,105 @@
-import logo from './logo.svg';
+import { useEffect, useState, useRef } from "react";
 import './App.css';
 
+
+
 function App() {
+  
+  const [clickedPad, setClickedPad] = useState('');
+  const audioPlayer = useRef();
+
+  useEffect(() => {
+    document.addEventListener("keydown", (e) =>{
+      handleSound(e.key.toUpperCase(),drumPads);
+    });
+  });
+
+
+  const drumPads = [
+    {
+      name:'Heater-1',
+      keyCode: 81,
+      text: "Q",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+    },
+    {
+      name:'Heater-2',
+      keyCode: 87,
+      text: "W",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+    },
+    {
+      name:'Heater-3',
+      keyCode: 69,
+      text: "E",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+    },
+    {
+      name:'Heater-4',
+      keyCode: 65,
+      text: "A",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+    },
+    {
+      name:'Clap',
+      keyCode: 83,
+      text: "S",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+    },
+    {
+      name:'Open-HH',
+      keyCode: 68,
+      text: "D",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+    },
+    {
+      name:'Kick-n-hat',
+      keyCode: 90,
+      text: "Z",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+    },
+    {
+      name:'Kick',
+      keyCode: 88,
+      text: "X",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+    },
+    {
+      name:'Closed-HH',
+      keyCode: 67,
+      text: "C",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+    }
+  ];
+
+ 
+  function handleSound(id, pads) {
+    const sound = document.getElementById(id);
+    sound.play();
+    const result = pads.filter((item)=>{
+      return item.text === id;
+    })
+    setClickedPad(result[0]);
+  }
+  
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div id='drum-machine'>
+          <div id='display'>{clickedPad.name}
+          <input type='range' volume className="form-range" onChange={(e) => {
+            audioPlayer.current.volume = e.target.value / 100;
+          }}></input></div>
+        <div className='pad-grid'>
+          {drumPads.map((pad) => (
+           <div className='drum-pad' id={pad.keyCode} onClick={() => handleSound(pad.text,drumPads)} key={pad.src}>
+              {pad.text}
+              <audio src={pad.src} id={pad.text} className='clip' ref={audioPlayer}></audio>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
